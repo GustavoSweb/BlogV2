1de1e89b98c0bb7b2cf5c0fd372262b92cbbbda0
gustavodasilvama10@gmail.com <gustavodasilvama10@gmail.com>
2022-12-20 17:28:12 -0300

    mm

--- categories/CategoriesControl.js
+++ categories/CategoriesControl.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const Router = express.Router()
-
+const Category = require("./categories")
 Router.get("/admin/categories/new", (req, res)=> {
   res.render("admin/categories/new.ejs")
 })
@@ -8,7 +8,10 @@ Router.get("/admin/categories/new", (req, res)=> {
 Router.get("/categories/save", (req, res)=> {
   const title = req.body.title
   if(title != undefined){
-    
+    Category.create({
+      title: title,
+      
+    })
   }else{
     res.redirect("/admin/categories/new")
   }
