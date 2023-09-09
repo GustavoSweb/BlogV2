da8251458dc2e0ad145a5e8c2853ccbe749cf163
Gustavo <96173358+GustavoSweb@users.noreply.github.com>
2023-09-09 20:40:49 -0300

    Update databases.js

--- database/databases.js
+++ database/databases.js
@@ -1,9 +1,5 @@
 const sequelize= require("sequelize")
 
-const connection = new sequelize ('blog', 'u0_a682', '123456',{
-  host: 'localhost',
-  dialect: 'mysql',
-  timezone: "-03:00"
-})
+const connection = new sequelize ("postgres://lcnbqxeb:pRbN9UxwpXoBrqPL7Yb7jW-DNhHBjaNM@babar.db.elephantsql.com/lcnbqxeb")
 
 module.exports = connection;
